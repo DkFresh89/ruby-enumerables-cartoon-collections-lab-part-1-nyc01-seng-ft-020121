0921a0ac1df66c5561b78bb0794155ac9228c563
Doug K <doug.kerzner@gmail.com>
2021-01-19 18:31:08 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def greet_characters(array)
   #
   # Print a custom greeting for each element
   array.each do |greeting|
-    greeting = "Hello ", array[0], "!"
+    greeting = "Hello ", array[1], "!"
     print greeting
   end 
 end
